d730482acc86de544026fa2de16762c3eaaac2b3
NabiL Ashbat <nbl.dev21@gmail.com>
2023-02-25 23:24:28 +0300

    Completed assignment

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,37 @@
-import React from "react";
-import { Route, Switch } from "react-router-dom";
-import NavBar from "./NavBar";
-import Home from "./Home";
-import Actors from "./Actors";
-import Directors from "./Directors";
-import Movies from "./Movies";
+/** @format */
+
+import React from 'react'
+import { Route, Switch } from 'react-router-dom'
+
+// Navigation
+import NavBar from './NavBar'
+
+//pages
+import Home from './Home'
+import Actors from './Actors'
+import Directors from './Directors'
+import Movies from './Movies'
 
 function App() {
-  return <div>{/*{code here}*/}</div>;
+  return (
+    <div>
+      <NavBar />
+      <Switch>
+        <Route path='/movies'>
+          <Movies />
+        </Route>
+        <Route path='/directors'>
+          <Directors />
+        </Route>
+        <Route path='/actors'>
+          <Actors />
+        </Route>
+        <Route path='/'>
+          <Home />
+        </Route>
+      </Switch>
+    </div>
+  )
 }
 
-export default App;
+export default App
--- src/components/Movies.js
+++ src/components/Movies.js
@@ -1,8 +1,27 @@
-import React from "react";
-import { movies } from "../data";
+/** @format */
+
+import React from 'react'
+import { movies } from '../data'
 
 function Movies() {
-  return <div>{/*{code here}*/}</div>;
+  const moviess = movies.map((movie, i) => (
+    <div key={i}>
+      <h2>{movie.title}</h2>
+      <p>Runtime: {movie.time} min.</p>
+      <ul>
+        {movie.genres.map((genre, i) => (
+          <li key={i}>{genre}</li>
+        ))}
+      </ul>
+    </div>
+  ))
+
+  return (
+    <div>
+      <h1>Movies Page</h1>
+      {moviess}
+    </div>
+  )
 }
 
-export default Movies;
+export default Movies
